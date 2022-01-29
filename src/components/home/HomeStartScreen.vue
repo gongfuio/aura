14e3ed36ce11286b2244a089103c1f6a0ad8fccc
Olivier Lange <olange@hey.com>
2022-01-29 06:21:46 +0100

    Adaptation for our GFIO meetup (WIP)

--- src/components/home/HomeStartScreen.vue
+++ src/components/home/HomeStartScreen.vue
@@ -16,13 +16,12 @@
                         <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
                     </v-layout>
                 </v-img>
-            </v-flex> 
+            </v-flex>
             <v-flex xs12 sm7 md8 lg8 xl8 class="">
-                <p class="google-font mb-1" style="font-weight: 350;color: #616161;font-size:300%"><b>Make good things 
-                    <span style="color: #1a73e8;">together</span>.</b>
+                <p class="google-font mb-1" style="font-weight: 350;color: #616161;font-size:300%"><strong>{{chapterDetails.ChapterShortDescription}}</strong>
                 </p>
                 <p class="google-font mt-0" style="font-size:180%">{{chapterDetails.ChapterName}}</p>
-                <p class="google-font" style="font-size:110%">{{chapterDetails.ChapterShortDescription}}</p>
+                <p class="google-font" style="font-size:110%">{{chapterDetails.ChapterLongDec}}</p>
                 <p class="google-font" style="font-size:110%;color:#9e9e9e">
                     <span v-for="(item,i) in chapterDetails.ChapterHashTags" :key="i">
                         #{{item}} &nbsp;
